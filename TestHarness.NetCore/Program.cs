de9aaa444eb5f4472cab0f2c8342f7c64de10c0e
Joel Hammond-Turner <joel.hammond-turner@landmark.co.uk>
2021-02-22 09:44:04 +0000

    Skeleton solution with "Hello Generated World" working in NetCore and NetFx

---
+++ TestHarness.NetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CodingBindingRedirects.TestHarness
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CodedBindingRedirects.BindingRedirects.Apply();
+
+            await Task.Delay(1000);
+
+            Console.WriteLine("\nPress ENTER to quit.");
+        }
+    }
+}
